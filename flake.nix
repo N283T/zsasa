03b8b400b8019b51a59400a24f3aab6a20732346
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-11 13:11:29 +0900

    feat: add flake.nix for nix-based CLI installation
    Supports `nix run github:N283T/zsasa` and `nix profile install github:N283T/zsasa`
    on x86_64-linux, aarch64-linux, x86_64-darwin, aarch64-darwin.
    
    Uses nixpkgs zig 0.15.2. The zxdrfile dependency is pre-fetched via a
    fixed-output derivation (zig build --fetch) so the main build runs
    fully sandboxed with --system.

---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  description = "zsasa - fast SASA calculator written in Zig";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+
+          # Pre-fetch Zig dependencies as a fixed-output derivation.
+          # This runs `zig build --fetch` with network access and captures
+          # the resulting package cache directory ($ZIG_GLOBAL_CACHE_DIR/p).
+          zigDeps = pkgs.runCommand "zsasa-zig-deps"
+            {
+              src = ./.;
+              nativeBuildInputs = [ pkgs.zig ];
+              outputHashAlgo = "sha256";
+              outputHashMode = "recursive";
+              outputHash = "sha256-wxgdxQiNj7hOTagippwrDkeiZ5RZsagvJI67T28jf04=";
+            }
+            ''
+              export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
+              cp -r $src/. .
+              zig build --fetch
+              mv $ZIG_GLOBAL_CACHE_DIR/p $out
+            '';
+
+          zsasa = pkgs.stdenv.mkDerivation {
+            pname = "zsasa";
+            version = "0.2.3";
+
+            src = ./.;
+
+            nativeBuildInputs = [ pkgs.zig ];
+
+            dontConfigure = true;
+            dontFixup = true;
+
+            buildPhase = ''
+              export ZIG_GLOBAL_CACHE_DIR=$(mktemp -d)
+              zig build \
+                -Doptimize=ReleaseFast \
+                --system ${zigDeps} \
+                --prefix $out \
+                -j$NIX_BUILD_CORES
+            '';
+
+            installPhase = ''
+              # zig build --prefix already installed the binary
+              true
+            '';
+
+            meta = with pkgs.lib; {
+              description = "Fast solvent accessible surface area (SASA) calculator written in Zig";
+              homepage = "https://github.com/N283T/zsasa";
+              license = licenses.mit;
+              mainProgram = "zsasa";
+              platforms = platforms.unix;
+            };
+          };
+        in
+        {
+          packages = {
+            default = zsasa;
+            inherit zsasa;
+          };
+
+          apps.default = flake-utils.lib.mkApp {
+            drv = zsasa;
+          };
+        }
+      );
+}
